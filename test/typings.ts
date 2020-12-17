7c9a6cb0963ebf9adce17484d3d9a58d15d84855
Dino Fabrello <oleg@borodkin.ru>
2020-12-18 02:40:42 +0300

    Add manual type tests

---
+++ test/typings.ts
@@ -0,0 +1,24 @@
+import DbRecord2 = require("../src/DbRecord2");
+
+class TestRecord extends DbRecord2 {
+	static _table() {
+		return "tests." + TABLE_NAME;
+	}
+
+	static _locatefield() {
+		return "id";
+	}
+
+	static _keys() {
+		return ["field2", "field2,field3", "name,field2,field3"];
+	}
+
+	myLocal() {}
+}
+
+// The 'item' type should resolve to "TestRecord" with TS
+TestRecord.forEach({
+
+}, async (item) => {
+	tmp.myLocal();
+})
